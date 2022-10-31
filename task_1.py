0a75ce2f7f61e9666c81486b2c324507e8b1b586
Feck-it-im-in <115720738+Feck-it-im-in@users.noreply.github.com>
2022-10-31 21:53:54 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,18 @@
 def remove_parentheses(text):
-    return text
+    result = ""
+    is_inside = False
+    for letter in text:
+        if letter == "(":
+            is_inside = True
+            pass
+        elif letter == ")":
+            is_inside = True
+            pass
+        else:
+            result = result + letter
+
+    return result
+
+
+print(remove_parentheses("(Nie) jest tak zle."))
+# work in progress
