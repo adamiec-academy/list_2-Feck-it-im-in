661a833e17cdf36c591a10fc8caa93efe3865b04
Feck-it-im-in <115720738+Feck-it-im-in@users.noreply.github.com>
2022-10-31 22:57:09 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,1 +1,18 @@
-# Remove this comment to confirm that this task is done
+import turtle
+
+def inflacja(n):
+  for i in range(n):
+    turtle.forward(10)
+    turtle.left(90)
+    turtle.forward((2 * n) + (10 * i))
+    turtle.left(90)
+    turtle.forward(10)
+    turtle.left(90)
+    turtle.forward((2 * n) + (10 * i))
+    turtle.left(90)
+    turtle.penup()
+    turtle.forward(20)
+    turtle.pendown()
+    
+inflacja(n)
+#more or less done, haven't used the random library
