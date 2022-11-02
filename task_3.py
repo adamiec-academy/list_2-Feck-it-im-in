378aa4473fc43f6cefa05d93e7712d74af744101
Feck-it-im-in <115720738+Feck-it-im-in@users.noreply.github.com>
2022-11-02 20:25:38 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -2,11 +2,11 @@ def is_prime(n):
     if n > 1:
         for j in range(2, int(n / 2) + 1):
             if (n % j) == 0:
-                return "False"
+                return False
         else:
-            return "True"
+            return True
     else:
-        return "False"
+        return False
 
 
 def is_diabolic(n):
